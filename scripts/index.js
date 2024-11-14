1783cee27ad9fcecdac3128e58ddb5f1975b82ce
Kairat Baiboriev <baiborievk@yandex.ru>
2024-11-14 15:47:46 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardContainer = document.querySelector(".places__list");
+
+function addCard(cardName, imageLink) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = imageLink;
+  cardElement.querySelector(".card__image").alt = cardName;
+  cardElement.querySelector(".card__title").textContent = cardName;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", () => removeCard(cardElement));
+
+  cardContainer.append(cardElement);
+}
+
+function removeCard(card) {
+  card.remove();
+}
+
+initialCards.forEach((element) => {
+  addCard(element.name, element.link);
+});
